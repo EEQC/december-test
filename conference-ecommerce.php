71b1362cc54f1d3b96681d7c974e4c48a1b23da5
Ecole des entrepreneurs QC <34692694+EEQC@users.noreply.github.com>
2017-12-20 10:01:09 -0500

    Add files via upload

---
+++ conference-ecommerce.php
@@ -0,0 +1,125 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Language" content="fr-ca">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Conférence e-Commerce</title>
+<link rel="stylesheet" type="text/css" href="/css/ecommerce.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<!-- BEGIN GOOGLE TRACKING CODE -->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-845565-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : '//www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<!-- END GOOGLE TRACKING CODE -->
+
+</head>
+
+<body topmargin="10" link="#00AEEF" vlink="#C0C0C0" alink="#00AEEF">
+
+<div align="center">
+	  
+<div id="border">
+  <div align="center">
+    <center>
+	
+      <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse" bordercolor="#111111" width="100%">
+		 <tr>
+			<td width="100%" colspan="2">
+				<div id="logoSAJE"><a href="//www.sajeenaffaires.org/fr/" target="_blank" alt="Logo SAJE acompagnateur d'entrepreneurs" title="SAJE accompagnateur d'entrepreneurs">
+				<img border="0" src="../../logos/logo_saje_1985_renv.png" width="300" align="left" vspace="10" hspace="0"></a>
+
+				<div style="background-color:#FFEFC6; width:100px; height:15px; margin-top:15px; margin-right:10px;float:right;position: relative; right:0px"></div>
+				<div style="background-color:#FFE28F; width:100px; height:15px; margin-top:35px; margin-right:10px;float:right;position: relative; right:-110px"></div>
+				<div style="background-color:#FFCD03; width:100px; height:15px; margin-top:55px; margin-right:10px;float:right;position: relative; right:-220px"></div>
+
+				</div>
+			</td>
+		</tr>
+		<tr>
+          <td width="100%" colspan="2">
+          	<div id="blockCenter">
+				<div class="title">
+					<p align="justify">André Menand, Directeur de l’équipe services spécialisés en croissance d’entreprise sera présent en compagnie d’Anthony Vendrame et de Pierre-Olivier Verret de Poches & Fils <b>le 17 mai</b> à la Maison du Cinéma de Sherbrooke pour vous parler de e-commerce!</p>
+				</div>
+			</div>
+			</div>
+		  </td>
+		</tr>
+		<tr>
+			<td width="100%" height="100%" colspan="2">
+				 <div id="quote"></div>
+					<h2>Cette conférence met de l’avant l’importance d’être présent où le consommateur effectue ses achats. Les Québécois ont dépensés plus de 8,5 milliards de dollars en ligne durant l’année 2016. Que ce soit pour gérer votre boutique en ligne ou pour maximiser votre taux de conversion au travers d’une stratégie de marketing numérique efficace, venez nombreux pour écouter les meilleurs conseils des personnes derrière le succès de l’entreprise Poches&Fils.</h2>
+			</td>
+		</tr>
+		<tr>
+          <td width="100%" colspan="2">
+          	 <div id="blockCenter">
+				<div class="title">
+					<p align="justify"><b>André Menand<br>Directeur de l’équipe services spécialisés en croissance d’entreprise, SAJE accompagnateur d’entrepreneurs</b></p>
+					<p align="justify">André Menand cumule plus de 15 années d’expérience en développement d’entreprise, mise en marche´, exportation et vente. Au cours de sa carrière, il a su accompagner plus de 500 entrepreneurs dans leur développement, dont 120 dans leur projet a` l’international. Il est également directeur du projet Succès-Numérique et InfoSocioFinancement. Depuis 3 ans, André Menand est conférencier sur différents projets de développement économique en Amérique latine.</p>
+					</div>
+				</div>
+			</div>
+		  </td>
+		</tr>
+		<tr>
+          <td width="100%" colspan="2">
+          	 <div id="blockCenter">
+				<div class="title">
+					<p align="justify"><b>Anthony Vendrame<br>Fondateur, CEO et Directeur Ventes &Marketing, Poches & Fils</b></p>
+					<p align="justify">Jeune entrepreneur, Anthony Vendrame cumule les succès en marketing et en vente. À titre de fondateur de Poches & Fils, il propulse l’entreprise et fait plus que quadrupler le chiffre d’affaires entre le premier et le deuxième exercice. La force de l’entreprise? Avoir un taux de conversion de visiteurs en acheteurs deux fois supérieur à la moyenne du marché. Poches & Fils a remporté le Prix Reconnaissance eCommerce-Québec 2016 du Conseil québécois du commerce de détail, pour l’intégration des médias sociaux dans sa stratégie marketing.</p>
+					</div>
+				</div>
+			</div>
+		  </td>
+		</tr>
+		<tr>
+          <td width="100%" colspan="2">
+          	 <div id="blockCenter">
+				<div class="title">
+					<p align="justify"><b>Pierre-Olivier Verret<br>Consultant - Stratège marketing numérique, Poches & Fils</b></p>
+					<p align="justify">Pierre-Olivier Verret participe au succès de Poches & Fils à titre de spécialiste marketing numérique. Il a d’ailleurs mis en place les stratégies de gestion de communautés, doublant de ce fait les audiences Facebook et Instagram, passant respectivement de 30000 à 60000 et de 15000 à 30000 abonnés en moins d’un an. Se spécialisant en stratégies marketing numérique, il apporte également son expertise à plusieurs entreprises telles que : 1642 Colas, KaseMe, CVert, Jet7 Location ou Edward Smoked Meat.</p>
+					</div>
+				</div>
+			</div>
+		  </td>
+		</tr>
+		<tr>
+          <td width="100%" colspan="2">
+          	 <div id="blockCenter">
+				<div class="texte">
+					<p align="justify">Si vous voulez accroître vos ventes en ligne et profiter des meilleurs conseils pour faire la promotion de votre cybercommerce, inscrivez-vous maintenant en cliquant sur le boutton ci-dessous.</p>
+					<p align="center"><a target="_blank" href="https://www.eventbrite.ca/e/billets-la-plus-poches-des-conferences-rencontrez-les-champions-du-e-commerce-33709445860"><button class="myButton">Inscription</button></a></p>
+					</div>
+				</div>
+			</div>
+		  </td>
+		</tr>
+        <tr>
+          <td width="100%" align="center"><div id="blockCenter">
+			<p align="center" class="copyright">© SAJE accompagnateur d'entrepreneurs, 1985-2017. Tous droits réservés.</p></div>
+		  </td>
+        </tr>
+       </table>
+		
+    </center>
+  </div><!-- center -->
+ </div><!-- border -->
+</div><!-- center -->
+
+</body>
+
+</html>
